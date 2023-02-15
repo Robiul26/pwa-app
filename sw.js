8094702f12234524a703e6ca603fe7bd48c3cb04
Robiul Islam <50030153+Robiul26@users.noreply.github.com>
2023-02-15 20:48:11 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,65 @@
+const cacheName = "app-rsrs";
+const dynamicCacheName = "app-rsrs1";
+
+const assets = [
+    './',
+    './js/app.js',
+    './js/common.js',
+    './js/materialize.min.js',
+    './css/style.css',
+    './css/materialize.min.css',
+    './img/contact.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v139/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    './pages/default.html'
+];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size))
+            }
+        })
+    })
+}
+
+
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== cacheName)
+                .map(key => caches.delete())
+            )
+        })
+    )
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(e.request.url, fetchRes.clone())
+                    limitCacheSize(dynamicCacheName, 2);
+                    return fetchRes;
+                })
+            });
+        }).catch(() => {
+            if (e.request.url.indexOf('.html') > -1) {
+                return caches.match('pages/default.html')
+            }
+        })
+    );
+});
